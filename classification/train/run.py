b97cb814c99459296a423b9cf9216a7419856412
SunAhong1993 <sunyanfang01@baidu.com>
2019-09-19 15:04:04 +0800

    add pretrained config

--- classification/train/run.py
+++ classification/train/run.py
@@ -156,58 +156,8 @@ if __name__ == "__main__":
             exit(1)
 
     # get pretrained model
-    pretrained_url = {
-        "AlexNet": "http://paddle-imagenet-models-name.bj.bcebos.com/AlexNet_pretrained.tar",
-        "MobileNetV1_x0_25": "https://paddle-imagenet-models-name.bj.bcebos.com/MobileNetV1_x0_25_pretrained.tar",
-        "MobileNetV1_x0_5": "https://paddle-imagenet-models-name.bj.bcebos.com/MobileNetV1_x0_5_pretrained.tar",
-        "MobileNetV1_x0_75": "https://paddle-imagenet-models-name.bj.bcebos.com/MobileNetV1_x0_75_pretrained.tar",
-        "MobileNetV1_x1_0": "http://paddle-imagenet-models-name.bj.bcebos.com/MobileNetV1_pretrained.tar",
-        "MobileNetV2_x0_25": "https://paddle-imagenet-models-name.bj.bcebos.com/MobileNetV2_x0_25_pretrained.tar",
-        "MobileNetV2_x0_5": "https://paddle-imagenet-models-name.bj.bcebos.com/MobileNetV2_x0_5_pretrained.tar",
-        "MobileNetV2_x0_75": "https://paddle-imagenet-models-name.bj.bcebos.com/MobileNetV2_x0_75_pretrained.tar",
-        "MobileNetV2_x1_0": "https://paddle-imagenet-models-name.bj.bcebos.com/MobileNetV2_pretrained.tar",
-        "MobileNetV2_x1_5": "https://paddle-imagenet-models-name.bj.bcebos.com/MobileNetV2_x1_5_pretrained.tar",
-        "MobileNetV2_x2_0": "https://paddle-imagenet-models-name.bj.bcebos.com/MobileNetV2_x2_0_pretrained.tar",
-        "MobileNetV3_small_x1_0": "https://paddle-imagenet-models-name.bj.bcebos.com/MobileNetV3_small_x1_0_pretrained.tar",
-        "ResNet18": "https://paddle-imagenet-models-name.bj.bcebos.com/ResNet18_pretrained.tar",
-        "ResNet34": "https://paddle-imagenet-models-name.bj.bcebos.com/ResNet34_pretrained.tar",
-        "ResNet50": "http://paddle-imagenet-models-name.bj.bcebos.com/ResNet50_pretrained.tar",
-        "ResNet101": "http://paddle-imagenet-models-name.bj.bcebos.com/ResNet101_pretrained.tar",
-        "ResNet152": "https://paddle-imagenet-models-name.bj.bcebos.com/ResNet152_pretrained.tar",
-        "ResNet50_vc": "https://paddle-imagenet-models-name.bj.bcebos.com/ResNet50_vc_pretrained.tar",
-        "ResNet18_vd": "https://paddle-imagenet-models-name.bj.bcebos.com/ResNet18_vd_pretrained.tar",
-        "ResNet34_vd": "https://paddle-imagenet-models-name.bj.bcebos.com/ResNet34_vd_pretrained.tar",
-        "ResNet50_vd": "https://paddle-imagenet-models-name.bj.bcebos.com/ResNet50_vd_pretrained.tar",
-        "ResNet101_vd": "https://paddle-imagenet-models-name.bj.bcebos.com/ResNet101_vd_pretrained.tar",
-        "ResNet152_vd": "https://paddle-imagenet-models-name.bj.bcebos.com/ResNet152_vd_pretrained.tar",
-        "ResNet200_vd": "https://paddle-imagenet-models-name.bj.bcebos.com/ResNet200_vd_pretrained.tar",
-    }
-    fc_name = {
-        "AlexNet": ["fc8_offset", "fc8_weights"],
-        "MobileNetV1_x0_25": ["fc7_weights", "fc7_offset"],
-        "MobileNetV1_x0_5": ["fc7_weights", "fc7_offset"],
-        "MobileNetV1_x0_75": ["fc7_weights", "fc7_offset"],
-        "MobileNetV1_x1_0": ["fc7_weights", "fc7_offset"],
-        "MobileNetV2_x0_25": ["fc10_weights", "fc10_offset"],
-        "MobileNetV2_x0_5": ["fc10_weights", "fc10_offset"],
-        "MobileNetV2_x0_75": ["fc10_weights", "fc10_offset"],
-        "MobileNetV2_x1_0": ["fc10_weights", "fc10_offset"],
-        "MobileNetV2_x1_5": ["fc10_weights", "fc10_offset"],
-        "MobileNetV2_x2_0": ["fc10_weights", "fc10_offset"],
-        "MobileNetV3_small_x1_0": ["fc_weights", "fc_offset"],
-        "ResNet18": ["fc_0.w_0", "fc_0.b_0"],
-        "ResNet34": ["fc_0.w_0", "fc_0.b_0"],
-        "ResNet50": ["fc_0.w_0", "fc_0.b_0"],
-        "ResNet101": ["fc_0.w_0", "fc_0.b_0"],
-        "ResNet152": ["fc_0.w_0", "fc_0.b_0"],
-        "ResNet50_vc": ["fc_0.w_0", "fc_0.b_0"],
-        "ResNet18_vd": ["fc_0.w_0", "fc_0.b_0"],
-        "ResNet34_vd": ["fc_0.w_0", "fc_0.b_0"],
-        "ResNet50_vd": ["fc_0.w_0", "fc_0.b_0"],
-        "ResNet101_vd": ["fc_0.w_0", "fc_0.b_0"],
-        "ResNet152_vd": ["fc_0.w_0", "fc_0.b_0"],
-        "ResNet200_vd": ["fc_0.w_0", "fc_0.b_0"],
-    }
+    from pretrained_model_config import pretrained_url, fc_name
+
     if settings.use_pretrained and settings.checkpoint is None:
         part = pretrained_url[settings.model].split("/")
         path = os.path.join("./pretrained", part[-1][:-4])
