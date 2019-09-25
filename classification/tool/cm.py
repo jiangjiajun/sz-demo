9e41eeaa431673f9410d4804567009fea0856039
cjt222 <18222160892@163.com>
2019-09-25 04:11:56 +0000

    update format in cm.py

--- classification/tool/cm.py
+++ classification/tool/cm.py
@@ -5,6 +5,7 @@ predict.txt: predict_label true_label
 from sklearn.metrics import confusion_matrix
 import matplotlib.pyplot as plt
 import numpy as np
+
 #load labels.
 labels = []
 file = open('labels.txt', 'r')
@@ -15,6 +16,7 @@ file.close()
 
 y_true = []
 y_pred = []
+
 #load true and predict labels.
 file = open('predict.txt', 'r')
 lines = file.readlines()
@@ -39,10 +41,7 @@ cm = confusion_matrix(y_true, y_pred)
 np.set_printoptions(precision=2)
 cm_n = cm.astype('float')
 plt.figure(figsize=(12,8), dpi=120)
-#set the fontsize of label.
-#for label in plt.gca().xaxis.get_ticklabels():
-#    label.set_fontsize(8)
-#text portion
+
 ind_array = np.arange(len(labels))
 x, y = np.meshgrid(ind_array, ind_array)
 
@@ -50,6 +49,7 @@ for x_val, y_val in zip(x.flatten(), y.flatten()):
     c = cm_n[y_val][x_val]
     if (c > 0.01):
         plt.text(x_val, y_val, "%0.2d" %(c,), color='red', fontsize=14, va='center', ha='center')
+
 #offset the tick
 plt.gca().set_xticks(tick_marks, minor=True)
 plt.gca().set_yticks(tick_marks, minor=True)
